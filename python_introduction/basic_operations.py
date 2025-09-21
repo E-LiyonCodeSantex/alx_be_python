b2e279c0b627aa77577d9f7f89abac8556467b92
E-LiyonCodeSantex <eelyondesign@gmail.com>
2025-09-21 09:20:27 +0100

    0. Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> number1 = 10
+>>> number2 = 5
+>>> Addition = number1 + number2
+>>> Subtraction = number1 - number2
+>>> Multiplication = number1 * number2
+>>> print("Addition of" number1 "and" number2 "is" Addition)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print("Adition of", number1, "and", number2, "is", Addition)
+Adition of 10 and 5 is 15
+>>> print("Subtraction of", number1, "and", number2, "is", Subtraction)
+Subtraction of 10 and 5 is 5
+>>> print("Multiplication of", number1, "and", number2, "is", Multiplication)
+Multiplication of 10 and 5 is 50
