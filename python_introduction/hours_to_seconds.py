fc1d4d82648ec02094d44bdd04cc4faf045024bd
E-LiyonCodeSantex <eelyondesign@gmail.com>
2025-09-21 09:52:26 +0100

    3. Convert Hours to Seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+# value
+hours = 2
+
+#Calculate how many seconds in the hour(s)
+seconds = 60 * 60 * 2
+
+#print result
+#[hours] hour(s) is [seconds] seconds.
+print(f"{hours} hour(s) is {seconds} seconds")
