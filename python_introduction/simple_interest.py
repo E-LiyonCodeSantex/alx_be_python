abed7c1f9257069ea639399664c85c7a672073b9
E-LiyonCodeSantex <eelyondesign@gmail.com>
2025-09-21 09:33:16 +0100

    . Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Values
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculate the simple interest rate
+interest = principal * rate * time
+
+# Print result
+print("The simple interest is:", interest)
